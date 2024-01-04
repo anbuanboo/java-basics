20ef8c111cb90ea68631e4ece9835ea1ae7a90fc
Venugopal S <Venugopal S@LAPTOP-B4UV1AR7>
2024-01-04 09:59:29 +0530

    "java basics"

---
+++ Anbubasicofjava/src/simpleprojavapack/Datatype.java
@@ -0,0 +1,13 @@
+package simpleprojavapack;
+
+public class Datatype {
+public static void main(String[]args) {
+	
+	
+	String first="black is my favourite color";
+	System.out.println(first.toLowerCase());
+	System.out.println(first.toUpperCase());	
+	
+	
+}	
+}
